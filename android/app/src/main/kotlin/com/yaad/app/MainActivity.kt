c879824327478063c693bc1490ef69932e731165
Aniket Bhatia <bhatiaaniket1@gmail.com>
2026-08-29 11:47:17 +0530

    fix: update Android compile SDK for current Flutter plugins

---
+++ android/app/src/main/kotlin/com/yaad/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.yaad.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/yaad/app/yadd/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.yaad.app.yadd
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
